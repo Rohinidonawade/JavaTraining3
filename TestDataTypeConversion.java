e291625da69174b543aa936d5a6b77e366d3832e
Rohinidonawade <105695493+Rohinidonawade@users.noreply.github.com>
2022-05-18 17:55:31 +0530

    Add files via upload

---
+++ TestDataTypeConversion.java
@@ -0,0 +1,39 @@
+package com.vstl.javatraining;
+
+public class TestDataTypeConversion {
+
+	public static void main(String[] args) {
+		stringDataTypeConversion objstringDataTypeConversion = new stringDataTypeConversion();
+		objstringDataTypeConversion.girlsMarriageAge("19");
+		
+		     System.out.println("***********");
+		
+		objstringDataTypeConversion.govertPSIExamCutOff("34");
+		
+	         System.out.println("***********");
+
+		
+		objstringDataTypeConversion.hdfcBankGivingLoan("true");
+		
+	         System.out.println("***********");
+	         
+	    objstringDataTypeConversion.belgaumDistrictUnivercity("Rani");
+	    
+	         System.out.println("***********");
+
+	    //IntegerDataTypeConversion
+	         
+		IntegerDataTypeConversion objIntegerDataTypeConversion = new IntegerDataTypeConversion();
+		objIntegerDataTypeConversion.boysMarriageAge(20);
+		
+	         System.out.println("***********");
+	         
+	   objIntegerDataTypeConversion.BangloreCityTemperature(30);
+	   
+             System.out.println("***********");
+
+	   
+	   objIntegerDataTypeConversion.puneCityTotalHospital('P');
+      }
+
+}
